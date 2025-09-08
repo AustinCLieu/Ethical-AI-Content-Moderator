decaa362000b07212a3823523bcb25e9f1b8dfc0
AustinCLieu <asianonrice@gmail.com>
2025-09-07 20:17:44 -0700

    Actually now added content.service to use DTO and queue moderation job. Thought I did already. Will add queue soon

--- api/src/content/content.service.ts
+++ api/src/content/content.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { jobsQ } from '../queue';
+import { CreateContentDto } from './dto/create-content.dto';
+
+const prisma = new PrismaClient();
 
 @Injectable()
-export class ContentService {}
+export class ContentService {
+    async create(dto: CreateContentDto) {
+        return prisma.content.create({
+            data: {
+                orgId: dto.orgId,
+                text: dto.text,
+                lang: dto.lang ?? 'en'
+            },
+        });
+    }
+
+    async enqueueModeration(job: { contentId: string; text: string; lang: string }) {
+        await jobsQ.add('moderate', job);
+    }
+}
